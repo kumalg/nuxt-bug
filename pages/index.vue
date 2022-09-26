708ab0a5ab530bebcfc9e390479c783b3e224294
Kamil Golec <kamil.golec@eobuwie.com.pl>
2022-09-26 14:03:56 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    INDEX {{ data.foo }}
+</div>
+</template>
+
+<script>
+export default {
+    async setup() {
+        const { data } = await useAsyncData(() => {
+            return {
+                foo: 'bar'
+            }
+        })
+
+        useHead({
+            title: data.value.foo
+        })
+
+        return {
+            data
+        }
+    }
+}
+</script>
